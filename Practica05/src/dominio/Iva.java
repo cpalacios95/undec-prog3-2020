40d3ff7fbd41b01b1f8214ddcb53ee3752b1c1cd
Cristian Palacios <cristianpalacios95@gmail>
2020-04-22 21:32:10 -0300

    Creacion de la clase Iva

---
+++ Practica05/src/dominio/Iva.java
@@ -0,0 +1,20 @@
+package dominio;
+
+public class Iva {
+
+	private int valor;
+	
+	public Iva() {
+		this.valor=21;
+	}
+
+	public int getValor() {
+		return valor;
+	}
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+	
+	
+}
